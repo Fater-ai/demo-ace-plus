df46181db8a241162830ffc114b11c930c6435d9
chaojie.mcj@alibaba-inc.com <yanwen.pyl@gmail.com>
2024-11-19 20:58:25 +0800

    delete the unne file

--- modules/model/diffusion/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) Alibaba, Inc. and its affiliates.
-
-from .diffusions import ACEDiffusion
-from .samplers import DDIMSampler
-from .schedules import LinearScheduler
