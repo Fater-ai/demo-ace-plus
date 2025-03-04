76148a4b1a8a09d71c557d00a41eeeda98c2b34c
chaojie mao <chaojiemao@users.noreply.huggingface.co>
2025-03-04 09:21:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import shlex
 import subprocess
 import importlib
 subprocess.run("rm -rf /data-nvme/zerogpu-offload/*", env={}, shell=True)
-subprocess.run(shlex.split('pip install scepter'))
+subprocess.run(shlex.split('pip install scepter --no-deps'))
 subprocess.run(shlex.split('pip install numpy==1.26'))
 from scepter.modules.transform.io import pillow_convert
 from scepter.modules.utils.config import Config
